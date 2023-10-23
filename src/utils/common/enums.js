c9d1ca7c106a36e5108c1cb7d538e6ae7080ca03
Vivek Patil <vivekpatil1718@gmail.com>
2023-10-23 11:04:55 +0530

    Added Booking model with utils

---
+++ src/utils/common/enums.js
@@ -0,0 +1,17 @@
+const SEAT_TYPE={
+    BUSINESS:'business',
+    ECONOMY:'economy',
+    PREMIUM_ECONOMY:'premium_economy',
+    FIRST_CLASS:'first_class'
+}
+
+const BOOKING_STATUS={
+    BOOKED:'booked',
+    CANCELLED:'cancelled',
+    INITIATED:'initiated',
+    PENDING:'pending'
+}
+module.exports={
+    SEAT_TYPE,
+    BOOKING_STATUS
+}
